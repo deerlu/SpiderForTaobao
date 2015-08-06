f5df75bf6b5cbe6337e7e804c35dc78f14c29469
Ludi <dlu.npu@gmail.com>
2015-08-06 19:01:46 +0800

    添加获取指定数量的宝贝成交列表功能，不再只获取第一页
    1.修改函数名称，使其更贴切
    2.添加函数get_rest_of_pages_products，用来获取剩下若干页的宝贝
    3.修改settings.py中的DOWNLOAD_DELAY值为1.5，该延迟时间并没有触发淘宝的anti-spider机制

--- SpiderForTaobao/spiders/taobao_spider.py
+++ SpiderForTaobao/spiders/taobao_spider.py
@@ -7,7 +7,7 @@ from SpiderForTaobao.items import SpiderfortaobaoItem
 
 class TaobaoSpider(Spider):
 	name = "taobao"
-	allowed_domains = ['www.taobao.com','item.taobao.com','detailskip.taobao.com']
+	allowed_domains = ['www.taobao.com','item.taobao.com','detailskip.taobao.com','s.taobao.com']
 
 	headers = {
 		"Accept": "*/*",
@@ -28,42 +28,72 @@ class TaobaoSpider(Spider):
 	# 在www.taobao.com搜索keyword指定的关键字，爬取并创建request对象
 	def start_requests(self):
 		url = "https://s.taobao.com/search?q=%s&imgfile=&commend=all&ssid=s5-e&search_type=item&sourceId=tb.index&spm=a21bo.7724922.8452-taobao-item.2&initiative_id=tbindexz_20150802" % self.keyword
-		return [Request(url, callback=self.get_count_urls)]
+		return [Request(url, callback=self.get_first_page_products)]
 
-	# 获取搜索结果列表里的前count个宝贝链接，存储到start_urls中
-	def get_count_urls(self, response):
+	# 获取搜索结果列表第一页里的所有宝贝，如果小于要获取的宝贝数量，则调用get_rest_of_pages_products以获取足够的宝贝；否则调用parse_every_product对每个宝贝进行处理
+	def get_first_page_products(self, response):
 		for sel in response.xpath('//script'):
 			if "g_page_config = " in sel.extract():
 				jsonStr =  sel.re(r"g_page_config\s=\s.*;")[0][16:-1]
 				jsonData = json.loads(jsonStr)
-				# 获取该页的宝贝id列表
+				# 将该页的所有宝贝id添加到idList中
 				idList = jsonData["mainInfo"]["traceInfo"]["traceData"]["allNids"]
 				# 搜索到的宝贝总数量
 				totalProducts = jsonData["mainInfo"]["traceInfo"]["traceData"]["totalHits"]
-				# 该页的宝贝总数量
-				currentProducts = jsonData["mainInfo"]["traceInfo"]["traceData"]["auctionReturnNum"]
 				# 搜索结果总页数
 				totalPage = jsonData["mods"]["sortbar"]["data"]["pager"]["totalPage"]
 				# 当前页数
 				currentPage = jsonData["mods"]["sortbar"]["data"]["pager"]["currentPage"]
 				break
 
+		
 		# 如果搜索到的总宝贝数比要获取的宝贝数量少，则修改要获取的宝贝数量为总宝贝数
-		if totalProducts < self.count:
+		if int(totalProducts) < int(self.count):
 			self.count = totalProducts
 
-		currentcount = 0
-		for id in idList:
-			currentcount = currentcount + 1
-			if(currentcount > self.count):
-				break
-			else:
+		if len(idList) >= int(self.count):
+			# 调用parse_every_product对self.count个宝贝进行处理
+			for id in idList[0:int(self.count)]:
 				url = "https://item.taobao.com/item.htm?spm=a230r.1.14.1.bNjCbf&id=%s&ns=1&abbucket=10#detail" % id
 				item = SpiderfortaobaoItem()
 				#存储该宝贝id到item中
 				item["productId"] = id
 				item["dealRecords"] = []
 				yield Request(url,meta={'item': item},callback=self.parse_every_product)
+		elif int(currentPage) < int(totalPage):
+			# 获取下一页的宝贝
+			currentPage = currentPage + 1
+			dateVal =(currentPage - 1) * 44
+			url = "https://s.taobao.com/search?data-key=s&data-value=%s&ajax=true&_ksTS=1438785627718_769&callback=jsonp770&ie=utf8&initiative_id=staobaoz_20150805&stats_click=search_radio_all%%3A1&js=1&imgfile=&q=%%E7%%A8%%8B%%E5%%BA%%8F%%E4%%BB%%A3%%E5%%86%%99&suggest=history_4&_input_charset=utf-8&wq=&suggest_query=&source=suggest&bcoffset=4" % dateVal
+			yield Request(url,meta={'idList': idList, 'totalPage': totalPage, 'currentPage': currentPage},callback=self.get_rest_of_pages_products)
+
+	# 获取剩下若干页的宝贝
+	def get_rest_of_pages_products(self, response):
+		idList = response.meta['idList']
+		totalPage = response.meta['totalPage']
+		currentPage = response.meta['currentPage']
+
+		jsonStr = response.body[ response.body.index("(") + 1 : response.body.rindex(")") ]
+		jsonData = json.loads(jsonStr)
+		# 将该页的所有宝贝id添加到idList中
+		idList.extend(jsonData["mainInfo"]["traceInfo"]["traceData"]["allNids"])
+
+		while(len(idList) < int(self.count) and int(currentPage) < int(totalPage)):
+			# 获取下一页的宝贝
+			currentPage = currentPage + 1
+			dateVal =(currentPage - 1) * 44
+			url = "https://s.taobao.com/search?data-key=s&data-value=%s&ajax=true&_ksTS=1438785627718_769&callback=jsonp770&ie=utf8&initiative_id=staobaoz_20150805&stats_click=search_radio_all%%3A1&js=1&imgfile=&q=%%E7%%A8%%8B%%E5%%BA%%8F%%E4%%BB%%A3%%E5%%86%%99&suggest=history_4&_input_charset=utf-8&wq=&suggest_query=&source=suggest&bcoffset=4" % dateVal
+			yield Request(url,meta={'idList': idList, 'totalPage': totalPage, 'currentPage': currentPage},callback=self.get_rest_of_pages_products)
+
+		for id in idList[0:int(self.count)]:
+			# 调用parse_every_product对self.count个宝贝进行处理
+			url = "https://item.taobao.com/item.htm?spm=a230r.1.14.1.bNjCbf&id=%s&ns=1&abbucket=10#detail" % id
+			item = SpiderfortaobaoItem()
+			#存储该宝贝id到item中
+			item["productId"] = id
+			item["dealRecords"] = []
+			yield Request(url,meta={'item': item},callback=self.parse_every_product)
+
 	
 	# 针对每个宝贝，获取其所有成交记录
 	def parse_every_product(self, response):
@@ -77,13 +107,14 @@ class TaobaoSpider(Spider):
 				else:
 					print "Not found g_config.recordsApi"
 				break
+
 		item = response.meta['item']
 		# 获取该宝贝的标题
 		item["title"] = response.xpath('//title/text()').extract()[0]
 		url = 'https:%s&bid_page=1' % dealRecordsUrl
-		yield Request(url,headers = self.headers,meta={'item': item, 'dealRecordsUrl': dealRecordsUrl},callback=self.parse_every_record)
+		yield Request(url,headers = self.headers,meta={'item': item, 'dealRecordsUrl': dealRecordsUrl},callback=self.get_all_records)
 
-	def parse_every_record(self,response):
+	def get_all_records(self,response):
 		jsonData = json.loads(response.body)
 		# 判断是否有下一页成交记录
 		hasNext = jsonData["data"]["showBuyerList"]["hasNext"]
@@ -101,7 +132,7 @@ class TaobaoSpider(Spider):
 		#处理下一页
 		if hasNext:
 			url = 'https:%s&bid_page=%d' % (dealRecordsUrl, currentRecordPage + 1)
-			yield Request(url,headers = self.headers,meta={'item': item, 'dealRecordsUrl': dealRecordsUrl},callback=self.parse_every_record)
+			yield Request(url,headers = self.headers,meta={'item': item, 'dealRecordsUrl': dealRecordsUrl},callback=self.get_all_records)
 		else:
 			yield item
 		
